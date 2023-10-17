d178ae0ca4ab273df378bb285bf555c7f783265f
belalreda1 <148174495+belalreda1@users.noreply.github.com>
2023-10-17 05:58:19 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 
 async function get() {
-    var result =await fetch('https://forkify-api.herokuapp.com/api/search?q=pizza');
+    var result =await fetch('https://dummyjson.com/products');
     var data = await result.json();
-    var response = data.recipes
+    var response = data.products;
     var text = ""; 
     for(var i = 0 ; i<response.length; i++){
         var cartonna = `<div class="col-md-4 test text-center mb-3">
-        <img src="${response[i].image_url}" class = "img-fluid  belal" >
-        <h2>${response[i].recipe_id}</h2>
+        <img src="${response[i].images[0]}" class = "img-fluid belal" >
+        <h2>${response[i].id}</h2>
         <h4>${response[i].title}</h4>
        </div>`
        text = text + cartonna 
     }
- 
+
     document.querySelector(".test").innerHTML=text;
     
 
